2484843c8c6301921b72c3b4aefed7b49baaaf10
Dan Revel <dan@nopolabs.com>
2012-01-22 18:20:22 -0800

    WIP

--- src/test/java/com/nopolabs/rabbitmq/ha/it/IntegrationTest.java
+++ src/test/java/com/nopolabs/rabbitmq/ha/it/IntegrationTest.java
@@ -2,14 +2,17 @@ package com.nopolabs.rabbitmq.ha.it;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import net.joshdevins.rabbitmq.client.ha.HaConnectionFactory;
 import net.joshdevins.rabbitmq.client.ha.retry.AlwaysRetryStrategy;
+import net.joshdevins.rabbitmq.client.ha.retry.SimpleRetryStrategy;
 
 import org.apache.log4j.Logger;
 import org.junit.After;
@@ -20,17 +23,16 @@ import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.ShutdownSignalException;
 import com.rabbitmq.tools.Tracer;
 
 public class IntegrationTest {
 	
     private static final Logger LOG = Logger.getLogger(IntegrationTest.class);
 
-    private static final int TRACER_PORT = 5673;
+    private static final int PUBLISH_PORT = 5673;
+    private static final int CONSUME_PORT = 5674;
 	private static final String AMQP_HOST = ConnectionFactory.DEFAULT_HOST;
 	private static final int AMQP_PORT = ConnectionFactory.DEFAULT_AMQP_PORT;
 	
@@ -38,117 +40,99 @@ public class IntegrationTest {
 	private static final String TEST_QUEUE = "TEST-queue";
 	private static final String TEST_ROUTING_KEY = "TEST-routingKey";
 	
-	private TracerServer tracerServer;
-	private HaConnectionFactory haFactory;
-	private ConnectionFactory factory;
+	private TracerProxy publishProxy;
+	private TracerProxy consumeProxy;
+	
+	private ConnectionFactory publishFactory;
+	private ConnectionFactory consumeFactory;
+	
 	private ExecutorService executor;
 	
 	@Test
 	public void publisherTest() throws IOException {
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
 		
-		for (int i = 0; i < 10000; i++) {
-			String msg = "msg-" + i;
-			channel.basicPublish(TEST_EXCHANGE, TEST_ROUTING_KEY, null, msg.getBytes());
-			if (i == 9000) {
-				tracerServer.breakConnection();
-			}
+		PublishingClient p = new PublishingClient(publishFactory.newConnection(), TEST_EXCHANGE, TEST_ROUTING_KEY) {
+			@Override
+			void work() throws IOException {
+				for (int i = 0; i < 100000; i++) {
+					send(("msg-" + i).getBytes());
+				}
+				System.out.println("DONE");
+			}			
+		};
+		
+		executor.execute(p);
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
+		publishProxy.shutdown();
+		executor.execute(publishProxy);		
 		
-		channel.close();
-		connection.close();
+        while (!executor.isTerminated()) {} // wait for all jobs to complete
 	}
 	
-	abstract class Client implements Runnable {
-	    private Connection connection;
-	    protected Channel channel;
-
-	    Client() throws IOException {
-            open();
-	    }
-	    
-	    abstract void work() throws IOException;
-	    
-        public void run() {
-            try {
-                 work();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        
-        private void open() throws IOException {
-            connection = factory.newConnection();
-            channel = connection.createChannel();
-        }
-        
-        private void close() throws IOException {
-            channel.close();
-            connection.close();
-        }
-	    
+	@Test
+	public void consumerTest() throws IOException {
+		
+		ConsumingClient c = new ConsumingClient(consumeFactory.newConnection(), TEST_QUEUE) {
+			
+			List<String> rcvd = Collections.synchronizedList(new ArrayList<String>());
+			
+			@Override
+			void work() throws IOException {
+				while (rcvd.size() < 1000) {
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+						break;
+					}
+				}					
+			}
+			
+	        boolean receive(byte[] msgBytes) throws IOException {
+	        	rcvd.add(new String(msgBytes));
+	            return true;
+	        }
+		};
+		
+		executor.execute(c);
 	}
 	
-    abstract class TestPublisher extends Client {
-        private String exchangeName;
-        private String routingKey;
-        
-        TestPublisher(String exchangeName, String routingKey) throws IOException {
-            this.exchangeName = exchangeName;
-            this.routingKey = routingKey;
-        }
-        
-        void send(byte[] msgBytes) throws IOException {
-            channel.basicPublish(exchangeName, routingKey, null, msgBytes);
-        }
-    }
-        
-    abstract class TestConsumer extends Client {
-         
-        TestConsumer(String queueName) throws IOException {
-            channel.basicConsume(queueName, new DefaultConsumer(channel) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] msgBytes) throws IOException {
-                    receive(msgBytes);
-                }
-            });
-        }
-        
-        boolean receive(byte[] msgBytes) throws IOException {
-            return true;
-        }
-    }
-        
 	@Before 
 	public void setup() throws IOException, ClassNotFoundException {
 	    
-	    executor = Executors.newFixedThreadPool(10);
+		executor = Executors.newFixedThreadPool(10);
 	    
-		tracerServer = new TracerServer(TRACER_PORT, AMQP_HOST, AMQP_PORT);
-		tracerServer.startup();
+		publishProxy = new TracerProxy("P", PUBLISH_PORT, AMQP_HOST, AMQP_PORT);
+		executor.execute(publishProxy);
 		
-	    haFactory = new HaConnectionFactory();
-	    haFactory.setRetryStrategy(new AlwaysRetryStrategy());
-		factory = haFactory;
-		factory.setHost("localhost");
-		factory.setPort(TRACER_PORT);
+		consumeProxy = new TracerProxy("C", CONSUME_PORT, AMQP_HOST, AMQP_PORT);
+		executor.execute(consumeProxy);
+		
+		publishFactory = createConnectionFactory("localhost", PUBLISH_PORT);
+		consumeFactory = createConnectionFactory("localhost", CONSUME_PORT);
 		
 		teardownQueue(TEST_EXCHANGE, TEST_QUEUE, TEST_ROUTING_KEY);
 		setupQueue(TEST_EXCHANGE, TEST_QUEUE, TEST_ROUTING_KEY);
 	}
 	
+	ConnectionFactory createConnectionFactory(String connectHost, int connectPort) {
+		HaConnectionFactory factory = new HaConnectionFactory();
+		factory.setHost(connectHost);
+		factory.setPort(connectPort);
+		factory.setRetryStrategy(new SimpleRetryStrategy());
+		return factory;
+	}
+	
 	@After
 	public void teardown() throws IOException, InterruptedException {
 		Thread.sleep(500);
-		tracerServer.shutdown();
-//		teardownQueue(TEST_EXCHANGE, TEST_QUEUE, TEST_ROUTING_KEY);
+		publishProxy.shutdown();
+		consumeProxy.shutdown();
 		Thread.sleep(500);
 	}
 	
@@ -182,17 +166,19 @@ public class IntegrationTest {
 		connection.close();
 	}
 	
-	class TracerServer {
+	class TracerProxy implements Runnable {
 		
-		private Constructor<Tracer> tracerConstructor;
+		private String id;
 		private int listenPort;
 		private String connectHost;
 		private int connectPort;
-		private Thread thread;
+		private Constructor<Tracer> tracerConstructor;
 		private Tracer.Logger logger;
+		private ServerSocket serverSocket;
 		private Socket socket;
 		
-		TracerServer(int listenPort, String connectHost, int connectPort) throws ClassNotFoundException {
+		TracerProxy(String id, int listenPort, String connectHost, int connectPort) throws ClassNotFoundException {
+			this.id = id;
 			this.listenPort = listenPort;
 			this.connectHost = connectHost;
 			this.connectPort = connectPort;
@@ -214,50 +200,105 @@ public class IntegrationTest {
 			throw new RuntimeException("Could not find constructor: Tracer(Socket sock, String id, String host, int port, Logger logger)");
 		}
 
-		private void trace(Socket socket, String id) throws Exception {
-			Tracer tracer = tracerConstructor.newInstance(socket, id, connectHost, connectPort, logger);	
-			Thread thread = new Thread(tracer);
-			thread.start();
-            LOG.info(id + " started");
-		}
-		
-		void breakConnection() {
-			if (socket != null) {
-				try {
-					socket.close();
-					socket = null;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		
-		void startup() {
-	    	logger = new Tracer.AsyncLogger(System.out);
-	    	logger.start();
-	        thread = new Thread() {
-	        	@Override
-	        	public void run() {
-	    	        try {
-	    	            ServerSocket server = new ServerSocket(listenPort);
-	    	            int counter = 0;
-	    	            while (true) {
-	    	            	socket = server.accept();
-	    	            	String id = "Tracer-" + (counter++);
-	    	                trace(socket, id);
-	    	            }
-	    	        } catch (Exception e) {
-	    	            e.printStackTrace();
-	    	        }	        		
-	        	}
-	        };
-			thread.start();
+		public void run() {
+			logger = new Tracer.AsyncLogger(System.out);
+			try {
+			    serverSocket = new ServerSocket(listenPort);
+				int counter = 0;
+				while (true) {
+					counter++;
+					String tid = id + "-" + counter;
+                    System.out.println("Socket accept() " + tid);
+                    socket = serverSocket.accept();
+                    System.out.println("Creating tracer " + tid);
+                    Tracer tracer = tracerConstructor.newInstance(socket, tid, connectHost, connectPort, logger);
+                    System.out.println("Starting tracer " + tid);
+                    tracer.start();
+                    System.out.println("Started tracer " + tid);
+                }
+            } catch (Exception e) {
+                 logger.stop(); // will stop shared logger thread
+                 e.printStackTrace();
+            }					
 		}
 		
 		void shutdown() {
-            logger.stop();
-			thread.interrupt();
+			logger.stop();
+			try {
+				serverSocket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	abstract class Client implements Runnable {
+		
+	    private Connection connection;
+	    protected Channel channel;
+
+	    Client(Connection connection) throws IOException {
+            open(connection);
+	    }
+	    
+	    abstract void work() throws IOException;
+	    
+        public void run() {
+            try {
+                 work();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        private void open(Connection connection) throws IOException {
+            this.connection = connection;
+            this.channel = connection.createChannel();
+        }
+        
+        private void close() throws IOException {
+            channel.close();
+            connection.close();
+        }
+	    
+	}
+	
+    abstract class PublishingClient extends Client {
+        private String exchangeName;
+        private String routingKey;
+        
+        PublishingClient(Connection connection, String exchangeName, String routingKey) throws IOException {
+        	super(connection);
+            this.exchangeName = exchangeName;
+            this.routingKey = routingKey;
+        }
+        
+        void send(byte[] msgBytes) throws IOException {
+            channel.basicPublish(exchangeName, routingKey, null, msgBytes);
+        }
+    }
+        
+    abstract class ConsumingClient extends Client {
+         
+    	ConsumingClient(Connection connection, String queueName) throws IOException {
+        	super(connection);
+            channel.basicConsume(queueName, new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] msgBytes) throws IOException {
+                    receive(msgBytes);
+                }
+            });
+        }
+        
+        boolean receive(byte[] msgBytes) throws IOException {
+            return true;
+        }
+    }
+        
 }
