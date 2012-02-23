35e1623ef6028c084733ee0bcea89f91f1edc1c0
Dan Revel <drevel@gilt.com>
2012-02-23 10:56:41 -0800

    -h for tracer.sh

--- tracer.sh
+++ tracer.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "${1}" == "-h" ]; then
+    echo 'Usage: tracer.sh [<listenport> [<connecthost> [<connectport>]]]'
+    echo '   Serially traces connections on the <listenport>, logging'
+    echo '   frames received and passing them to the connect host and port.'
+    exit
+fi
+
 M2_REPO=~/.m2/repository
 AMQP_JAR=$M2_REPO/com/rabbitmq/amqp-client/2.7.1/amqp-client-2.7.1.jar
 IO_JAR=$M2_REPO/commons-io/commons-io/1.4/commons-io-1.4.jar
